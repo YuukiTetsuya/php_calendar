92548b04975957c17a9451cd13a1d59b1e88a07f
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-17 17:58:28 +0900

    カレンダー模型を作成

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="ja">
+   <head>
+     <meta charset="utf-8">
+     <title>PHPカレンダー</title>
+   </head>
+   <body>
+     <table>
+       <thead>
+         <tr>
+           <th><a href="">&laquo;</a></th>
+           <th colspan="5">August 2015</a></th>
+           <th><a href="">&laquo;</a></th>
+         </tr>
+       </thead>
+       <tbody>
+       </tbody>
+       <tfoot>
+         <tr>
+           <th colspan="7"><a href="">Today</a></th>
+         </tr>
+       </tfoot>
+     </table>
+   </body>
+ </html>

16df2ce985c041450265a7a9ebe6a469e4a6b8ae
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-17 18:14:14 +0900

    th,td,colspanでカレンダーの模型を作成 colspanは横方向に結合 th はヘッダー tdはセル

--- index.php
+++ index.php
@@ -18,6 +18,33 @@
          </tr>
        </thead>
        <tbody>
+         <tr>
+           <th>SUN</th>
+           <th>MON</th>
+           <th>Tue</th>
+           <th>Wed</th>
+           <th>Thu</th>
+           <th>Fri</th>
+           <th>Sat</th>
+         </tr>
+         <tr>
+           <td class="youbi_0">1</td>
+           <td class="youbi_1">2</td>
+           <td class="youbi_2">3</td>
+           <td class="youbi_3">4</td>
+           <td class="youbi_4 today">5</td>
+           <td class="youbi_5">6</td>
+           <td class="youbi_6">8</td>
+         </tr>
+         <tr>
+           <td class="youbi_0">30</td>
+           <td class="youbi_1">31</td>
+           <td class="gray">1</td>
+           <td class="gray">2</td>
+           <td class="gray">3</td>
+           <td class="gray">4</td>
+           <td class="gray">5</td>
+         </tr>
        </tbody>
        <tfoot>
          <tr>

038f05c938b19cf7c326ac809f06240e27adfc71
YuukiTetsuya <yuukitetsuyanet@gmail.com>
2019-06-17 21:42:30 +0900

    DateTimeオブジェクトをに代入し、現在の日付をformatメソッドを取得してに代入

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
+// 現在の年/月を$tに代入し、DateTimeオブジェクトを$thisMonthに代入し、月/年をF/Yでformatメソッドで取得し、$yearMonthに代入
+
+$t = '2015-09';
+$thisMonth = new DateTime($t); //2015-0801
+$yearMonth = $thisMonth->format('F Y');
+
 $tail = '';
-$lastDayOfPrevMonth = new DateTime('last day of previous month');
+$lastDayOfPrevMonth = new DateTime('last day of '. $yearMonth . '-1 month');
 while($lastDayOfPrevMonth->format('w') < 6){
   $tail = sprintf('<td class="gray">%d</td>', $lastDayOfPrevMonth->format('d')) .
   $tail;
@@ -12,9 +18,9 @@ $body = '';
 // 特定の期間の日付オブジェクトを作る
 $period = new DatePeriod(
 // 1日から末日までの日付を作る
-  new DateTime('first day of this month'),
+  new DateTime('first day of '. $yearMonth),
   new DateInterval('P1D'),
-  new DateTime('first day of next month')
+  new DateTime('first day of '. $yearMonth . ' +1 month')
 );
 
 // body変数に連結し、書式付きで文字列を作るsprintfで、tdタグの中にd要素(1〜末日)を入れる
@@ -30,7 +36,7 @@ foreach ($period as $day) {
 // DateTimeオブジェクトで、0(日曜)~6(土曜)が、日曜り大きい月曜〜土曜であれば、
 
 $head = '';
-$firstDayOfNextMonth = new DateTime('first day of next month');
+$firstDayOfNextMonth = new DateTime('first day of '. $yearMonth . ' +1 month');
 while($firstDayOfNextMonth->format('w') > 0){
   $head .= sprintf('<td class="gray">%d</td>', $firstDayOfNextMonth->format('d'));
   $firstDayOfNextMonth->add(new DateInterval('P1D'));
@@ -52,7 +58,7 @@ $html = '<tr>' . $tail. $body. $head . '</tr>';
        <thead>
          <tr>
            <th><a href="">&laquo;</a></th>
-           <th colspan="5">August 2019</a></th>
+           <th colspan="5"><?php echo $yearMonth; ?></a></th>
            <th><a href="">&raquo;</a></th>
          </tr>
        </thead>
